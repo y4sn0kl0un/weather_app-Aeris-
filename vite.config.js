a6755e5c72e28a8978b96e7680736dc3fa7a71ff
maxark02 <98244585+maxark02@users.noreply.github.com>
2025-11-25 14:13:41 +0900

    preview

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
     base: "/",
     server: {
         proxy: {
@@ -11,5 +11,14 @@ export default defineConfig({
                 rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
+        host: true
     },
+    preview: {
+        host: true,
+        strictPort: true,
+        port: 4173,
+        allowedHosts: [
+            "frontend-production-95bb.up.railway.app"
+        ]
+    }
 })
